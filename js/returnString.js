4d487c9d25d70d41d616a7dc325cc1ce6a3eb0fe
Maikku <michael.le210@gmail.com>
2022-05-14 10:15:06 -0400

    Return String Codewars compplete

---
+++ js/returnString.js
@@ -0,0 +1,11 @@
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+
+//input a string (name)
+//return the string inside of a statement "Hello, <name> how are you doing today?"
+//(greet("Ryan"), "Hello, Ryan how are you doing today?");
+//(greet("Shingles"), "Hello, Shingles how are you doing today?");
+
+function greet(name){
+    return (`Hello, ${name} how are you doing today?`)
+  }
