2a6d11625c0aca0f537d74771c9269a58bc634e0
Maikku <michael.le210@gmail.com>
2022-05-20 20:05:13 -0400

    Covert Boolean to String Codewar complete

---
+++ js/convertBooleanToString.js
@@ -0,0 +1,10 @@
+// Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+
+// convert the given boolean value
+// return string representation
+// booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+// booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
+function booleanToString(b){
+    return b.toString()
+  }
