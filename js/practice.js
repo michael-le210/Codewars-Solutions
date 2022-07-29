6f2de9d93f250a285c88b4511f59330b788edc73
Maikku <michael.le210@gmail.com>
2022-07-28 22:33:25 -0400

    Study

--- js/practice.js
+++ js/practice.js
@@ -37,25 +37,3 @@
 //     return b.repeat(a)
 // }
 
-// p takes a string
-// r returns a new string with vowels removed
-// e the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-// p seperate string into array use forEach loop with a condition to push nonvowels then join to create a string again
-// p create variable for vowel use for loop to loop through string use condition to push nonvowels then join to create a string again
-
-
-function disemvowel(str) {
-    let vow = ['a','e','i','o','u','A','E','I','O','U'] 
-    let newA = []
-    for(let i of str){
-      if(!vow.includes(i)){
-        newA.push(i)
-      }
-      }
-
-
-    
-    return newA.join('');
-  }
-
-  console.log(disemvowel('This website is for losers LOL!'))
